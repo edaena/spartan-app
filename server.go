11909ae1a2beffdd2452746143082ddbaf3a955b
andrebriggs@users.noreply.github.com <andrebriggs@users.noreply.github.com>
2019-04-23 11:36:38 -0700

    Adding intial files

---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var appVersion = "0.1" //Default/fallback version
+var instanceNum int
+
+func getFrontpage(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().UTC()
+	fmt.Fprintf(w, "Hi! I'm instance %d running version %s of your application at %s\n", instanceNum, appVersion, t.Format("2006-01-02 15:04:05"))
+}
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func getVersion(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s\n", appVersion)
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	instanceNum = rand.Intn(1000)
+	http.HandleFunc("/", getFrontpage)
+	http.HandleFunc("/health", health)
+	http.HandleFunc("/version", getVersion)
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
